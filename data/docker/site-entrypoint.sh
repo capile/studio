884eb44f03248de1bd80cc4520e05c610a31c16e
Guilherme Capilé <capile@tecnodz.com>
2023-03-07 17:39:39 -0300

    site entrypoint

--- data/docker/site-entrypoint.sh
+++ data/docker/site-entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 if [ ! -d "$STUDIO_DATA/web/.git" ]; then
-    git clone STUDIO_SITE_REPO "$STUDIO_DATA/web"
+    git clone $STUDIO_SITE_REPO "$STUDIO_DATA/web"
 fi
 
-studio :assets
+studio :assets -v S site
 
 exec php-fpm
