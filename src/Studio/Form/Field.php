f4955cd0be6b13ee52c1f8c5e9e106958314253f
Guilherme Capilé <capile@tecnodz.com>
2024-12-09 17:18:45 -0300

    bugfix embedded forms errors;choices-js optimization

--- src/Studio/Form/Field.php
+++ src/Studio/Form/Field.php
@@ -702,6 +702,9 @@ class Field extends SchemaObject
                     }
                     if(!$F->validate($v)) {
                         $valid = false;
+                        if($ea = $F->getError()) {
+                            $errors[] = $ea;
+                        }
                     }
                     $value[$i] = $O;
                 } catch(Exception $e) {
